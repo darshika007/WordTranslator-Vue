37f0de2552320a411dbf5dee4492aa2926f36729
darshika07 <darshikapillai@gmail.com>
2019-02-26 15:55:31 +0530

    first commit

---
+++ src/components/TranslateForm/TranslateForm.vue
@@ -0,0 +1,48 @@
+<style>
+    @import '../TranslateForm/translateform.css';
+</style>
+<template>
+  <div class="row" id="translateForm">
+    <div class="col-md-6 col-md-offset-3">
+    
+    <form id="transForm" class="form-inline well" v-on:submit="formSubmit">
+        <input class="form-control" type="text" v-model="textToTranslate" placeholder="Enter a Word..">
+        <select class="form-control" v-model="language">
+            <option value="ru">Russian</option>
+            <option value="es">Spamish</option>
+            <option value="fr">French</option>
+            <option value="zh">Chinese</option>
+        </select>
+        
+        <input class="btn btn-primary" type="submit" value="Translate">
+    </form>
+    
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'translateForm',
+  data() {
+      return{
+          textToTranslate:'',
+          language:''
+      }
+  },
+  created(){
+      this.language='ru';
+  },
+  methods: {
+      formSubmit(e){
+          this.$emit('formSubmit', this.textToTranslate, this.language);
+          e.preventDefault();
+          
+      }
+  }
+  
+}
+
+</script>
+
---
+++ src/components/Translateoutput/Translateoutput.vue
@@ -0,0 +1,16 @@
+<style>
+    @import '../Translateoutput/translateoutput.css';
+</style>
+<template>
+  <h2 class="text-success">{{translateText}}</h2>
+</template>
+
+<script>
+
+export default {
+  name: 'translateOutput',
+  props: ['translateText']
+}
+
+</script>
+
